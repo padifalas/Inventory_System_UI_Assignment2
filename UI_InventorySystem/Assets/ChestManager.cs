244b47fd92c0de437dc0ab78dbc032cb28b658bf
padifalasanerd <163738111+padifalas@users.noreply.github.com>
2024-05-16 20:28:31 +0200

    UPGRADE CHEST SLOTS COMMIT

--- UI_InventorySystem/Assets/ChestManager.cs
+++ UI_InventorySystem/Assets/ChestManager.cs
@@ -1,86 +1,37 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI; 
 
 public class ChestManager : MonoBehaviour
 {
-    public List<GameObject> slots = new List<GameObject>();
-    public bool[] slotAvailability;
-    
-    public int initialSlotCount = 3; // Initial number of slots in the chest
-    public int maxSlotCount = 10; // Maximum number of slots in the chest
-    public GameObject chestSlotPrefab; // Prefab of the chest slot UI element
-    public Transform chestPanel; // Reference to the chest panel where slots will be instantiated
-    public Button upgradeButton; // Button to upgrade the chest
-    
-    private int currentSlotCount; // Current number of slots in the chest
+    public Transform chestPanel; 
+    public Button upgradeButton; 
 
+    public int currentSlotIndex = 0;
 
     public void Start()
     {
-        currentSlotCount = initialSlotCount;
-        InstantiateChestSlots();
-        CheckUpgradeAvailability();
-        
-        
-        // Initialize slot availability array
-        slotAvailability = new bool[slots.Count];
-        for (int i = 0; i < slots.Count; i++)
-        {
-            slotAvailability[i] = true; // All slots are initially available
-        }
-    }
+        // Disable upgrade button if all slots are already active
+        upgradeButton.interactable = currentSlotIndex < chestPanel.childCount;
 
-    public GameObject GetAvailableSlot()
-    {
-        // Find and return the first available slot in the chest
-        for (int i = 0; i < slots.Count; i++)
-        {
-            if (slotAvailability[i])
-            {
-                slotAvailability[i] = false; // Mark slot as unavailable
-                return slots[i];
-            }
-        }
-        // If no slot is available, return null or handle it according to your logic
-        return null;
-    }
-
-    public void MakeSlotAvailable(GameObject slot)
-    {
-        int index = slots.IndexOf(slot);
-        if (index != -1 && index < slotAvailability.Length)
-        {
-            slotAvailability[index] = true; // Mark slot as available
-        }
-        else
-        {
-            Debug.LogError("Slot not found or index out of range.");
-        }
-    }
-    
-    public void InstantiateChestSlots()
-    {
-        for (int i = 0; i < currentSlotCount; i++)
-        {
-            GameObject slot = Instantiate(chestSlotPrefab, chestPanel);
-            slot.name = "ChestSlot_" + i;
-        }
+        // Activate initial slots if needed (optional)
+        ActivateNextSlot();
     }
 
     public void UpgradeChest()
     {
-        if (currentSlotCount < maxSlotCount)
-        {
-            currentSlotCount++;
-            InstantiateChestSlots();
-            CheckUpgradeAvailability();
-        }
+        ActivateNextSlot();
+
+        // Disable upgrade button if all slots are now active
+        upgradeButton.interactable = currentSlotIndex < chestPanel.childCount;
     }
 
-    public void CheckUpgradeAvailability()
+    public void ActivateNextSlot()
     {
-        upgradeButton.interactable = currentSlotCount < maxSlotCount;
+        if (currentSlotIndex < chestPanel.childCount)
+        {
+            Transform slot = chestPanel.GetChild(currentSlotIndex);
+            slot.gameObject.SetActive(true);
+            currentSlotIndex++;
+        }
     }
 }
